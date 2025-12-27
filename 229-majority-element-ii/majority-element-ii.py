6300f9caa80ac3d524bcd39dffa0fe9c6fdf0110
Shmy <82426113+Shmy1234@users.noreply.github.com>
2025-12-27 01:24:06 -0500

    Time: 11 ms (30.05%) | Memory: 21.5 MB (52.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -5,7 +5,6 @@ class Solution:
         for n in nums:
             d[n] = 1 + d.get(n, 0)
         for k, v in d.items():
-            print(k, v)
             if v > len(nums)/3:
                 l.append(k)
         return l
